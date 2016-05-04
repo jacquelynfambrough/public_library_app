5553047daabf56ee9f9bb447bc3b2a2e9191ad31
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-04 11:35:47 -0700

    added show method

--- lib_app/app/controllers/libraries_controller.rb
+++ lib_app/app/controllers/libraries_controller.rb
@@ -10,4 +10,20 @@ class LibrariesController < ApplicationController
 
     render :new
   end
+
+  def create
+    @library = Library.create(library_params)
+
+    redirect_to libraries_path
+  end
+
+  def show
+    @library = Library.find_by_id(params[:id])
+    render :show
+  end
+
+  private
+  def library_params
+    params.require(:library).permit(:name, :floor_count, :floor_area)
+  end
 end
