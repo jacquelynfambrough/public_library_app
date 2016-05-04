42ce090b9eeaa2fa475a5a12c2700d690a229373
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-04 12:28:26 -0700

    created route for user to join library

--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -25,4 +25,8 @@ Rails.application.routes.draw do
 
   get "/users/:user_id/libraries", to: "library_users#index", as: "user_libraries"
 
+  post "/libraries/:library_id/users", to: "library_users#create", as: "library_users"
+
+
+
 end
